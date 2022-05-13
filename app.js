8730e27cb59af3782fe5b473b0aeb75d8c5e5020
vitalyBatishchev <batishchev.exceedteam@gmail.com>
2022-05-13 14:21:11 +0300

    fix just delete and update

--- app.js
+++ app.js
@@ -1,54 +1,57 @@
-const express = require('express');
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const app = express();
+const express = require ('express');
+const cors = require ("cors");
+const mongoose = require ("mongoose");
+// const { ObjectId } = require('mongodb');
+// const _id = ObjectId();
+const app = express ();
 
 const {Schema} = mongoose;
 
 const taskScheme = new Schema ({
   text: String,
   isChek: Boolean,
+  _id: mongoose.Schema.Types.ObjectId
 });
 
-const Task = mongoose.model("tasks", taskScheme);
+const Task = mongoose.model ("tasks", taskScheme);
 
-app.use(cors());
+app.use (cors());
 
 const url = "mongodb+srv://Vitaliy:ZAQwed123@cluster0.c5jod.mongodb.net/TodoList?retryWrites=true&w=majority";
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect (url, {useNewUrlParser: true, useUnifiedTopology: true});
 
-app.use(bodyParser.json());
+app.use (express.json());
 
-app.get("/allTasks", (req, res) => {
-  Task.find().then(result => {
-    res.send({data: result})
+app.get ("/allTasks", (req, res) => {
+  Task.find ().then (result => {
+    res.send ({data: result})
   });
 });
 
-app.post("/createTasks", (req, res) => {
-  const task = new Task(req.body);
-  task.save().then(result => {
-    res.send("task created")
-  })
-});
-
-app.delete("/deleteTasks/:id", (req, res) => {
-  const {id} = req.query;
-  Task.deleteOne({_id: id}).then(result => {
-    res.send(result)
+app.post ("/createTasks", (req, res) => {
+  const task = new Task (req.body);
+  task.save ();
+  Task.find ().then (result => {
+    res.send ({data: result})
   });
 });
 
-app.put("/updateTasks/:id", (req, res) => {
-  const {id} = req.body;
-  Task.updateOne({_id: id}).then(result => {
-    Task.find({_id: id}).then(result => {
-      res.send({data: result})
-    });
+app.delete ("/deleteTasks/:id", (req, res) => {
+  const {_id} = req.params;
+  Task.deleteOne({_id });
+  Task.find ().then (result => {
+    res.send ({data: result})
   });
 });
 
-app.listen(5000, () => {
-  console.log("Example app");
+app.put ("/updateTasks", (req, res) => {
+  const {text, isChek, _id} = req.body;
+  Task.updateOne ({_id }, {text, isChek});
+  Task.find ().then (result => {
+    res.send ({data: result})
+  });
+});
+
+app.listen (5000, () => {
+  console.log ("Example app");
 });
